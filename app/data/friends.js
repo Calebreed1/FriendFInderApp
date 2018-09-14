bb45f55773103640711125e3c65dc6ee8c80f19b
Caleb Reed <cwreed@yahoo.com>
2018-09-14 00:45:52 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Aretha Franklin",
+		photo: "http://www.gstatic.com/tv/thumb/persons/590/590_v9_bb.jpg",
+		scores: [5, 5, 3, 2, 1, 2, 5, 4, 1, 4]
+	},
+	{
+		name: "Etta James",
+		photo: "https://img.discogs.com/lNVALNSfH_qElyk-iPc9XEtO6Ho=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-89660-1272502871.jpeg.jpg",
+		scores: [3, 1, 5, 1, 2, 3, 1, 3, 2, 3]
+	},
+	{
+		name: "Ottis Redding",
+		photo: "http://www.gstatic.com/tv/thumb/persons/1435/1435_v9_ba.jpg",
+		scores: [4, 1, 2, 3, 3, 5, 1, 2, 1, 5]
+	},
+	{
+		name: "Elvis Presely",
+		photo: "https://img.gentside.de/article/480/tod/elvis-presley_5fc73042573fba8ce49a455c32642491f3d20ee7.jpg",
+		scores: [5, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Frank Sinatra",
+		photo: "http://www.imagesyoulike.com/images/v/32x24/v3547.jpg",
+		scores: [2, 1, 4, 1, 2, 4, 3, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
